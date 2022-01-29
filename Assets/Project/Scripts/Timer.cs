886505e267bd1cc93b2f093f0c2ee2a870a9f19f
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2022-01-29 00:31:23 -0500

    timer and ui working.

---
+++ Assets/Project/Scripts/Timer.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace GGJ
+{
+    public class Timer : MonoBehaviour
+    {
+        [SerializeField]
+        private float duration;
+
+        [SerializeField]
+        private float timer;
+
+        [SerializeField]
+        private TextMeshProUGUI display_Text;
+
+        public bool failure{ get; private set; }
+        bool enables = false;
+
+        private void Start()
+        {
+            ResetTimer();
+        }
+
+        private void Update()
+        {
+            
+                UpdateTimer();
+            
+            
+        }
+
+        private void UpdateTimer()
+        {
+            //we failed
+            //we update timer
+            //we set failure
+
+            if(failure==true)
+            {
+                FailedEvent();
+
+            }
+            if(timer > 0 && failure==false)
+            {
+                timer -= Time.deltaTime;
+                UpdateDisplay(timer);
+
+            }
+            if(timer <0.7)
+            {
+                //we failed if we get here so call the method to update
+                //and send events
+                failure = true;
+            }
+        }
+
+        private void UpdateDisplay(float time)
+        {
+            if(enables)
+            {
+                var minutes = Mathf.FloorToInt(time/60);
+            var seconds = Mathf.FloorToInt(time % 60);
+
+            var currentTime = ""+minutes+":" + seconds;
+            display_Text.SetText(currentTime);
+            }
+        }
+
+        private void ResetTimer()
+        {
+            timer = duration;
+            UpdateDisplay(timer);
+        }
+        private void FailedEvent()
+        {
+            
+            Debug.Log("we failed");
+        }
+
+        public void ToggleText()
+        {
+            if(enables == false)
+            {
+                
+                enables = true;
+                UpdateDisplay(timer);
+
+            } else if(enables == true)
+            {
+                
+                display_Text.SetText("");
+                enables= false;
+            }
+
+        }
+      
+    }
+}
